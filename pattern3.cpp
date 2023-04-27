782f83985f2736209ccf55992e62393a3b5f69c0
unknown <codewizard2003@gmail.com>
2023-04-27 23:18:04 +0530

    First commit

---
+++ pattern3.cpp
@@ -0,0 +1,27 @@
+//   A
+//  BC
+// DEF
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of rows: "<<endl;
+    cin>>n;
+    int i=1;
+    char ch='A';
+    while(i<=n){
+        int space=n-i;
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }int j=1;
+        while(j<=i){
+            cout<<ch;
+            ch++;
+            j=j+1;
+
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
