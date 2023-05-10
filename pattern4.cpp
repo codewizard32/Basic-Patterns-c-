82d8613eaf6fb594055c75771242bd26af0b6280
codewizard32 <131467398+codewizard32@users.noreply.github.com>
2023-05-10 18:41:27 +0530

    Update pattern4.cpp

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-int i, j, n;
-cin >> n;  // 'n' must be odd
-for(i = 0; i < n; i++)
-{
-for(j = 0; j < n; j++)
-{
-// left diagonal, right diagonal, top horizontal, bottom horizontal, left vertical, right vertical
-if(i == j || i + j == n - 1 || i == 0 || i == n - 1 || j == 0 || j == n - 1)
-cout << "*";
-else
-cout << " ";
-}
-cout << "\n";
-}
-return 0;
+    int n;
+    cout<<"Enter Number";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
 }
